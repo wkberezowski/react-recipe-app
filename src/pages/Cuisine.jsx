1a30147c3e728739b41fef10ca44e576bec723e3
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-18 17:06:50 +0200

    added cuisine pages

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+function Couisne() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+  const getCuisine = async (name) => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host':
+          'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+        'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
+      },
+    };
+    const api = await fetch(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?cuisine=${name}`,
+      options
+    );
+    const recipes = await api.json();
+
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+export default Couisne;
